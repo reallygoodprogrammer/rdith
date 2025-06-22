239778e24aea7afc30689671e65537bcd66f7705
reallygoodprogrammer <milesdustin45@gmail.com>
2025-06-22 12:24:48 -0700

    better help messaging

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,13 @@ use rand::Rng;
 
 #[derive(Parser)]
 struct Args {
+    #[arg(help = "input files for dithering")]
     input_file: String,
-    #[arg(long, default_value_t = String::from("output.png"))]
+    #[arg(long, default_value_t = String::from("output.png"), help = "name of output file")]
     output_file: String,
-    #[arg(long, default_value_t = String::from(""))]
-    layer: String,
-    #[arg(long, default_value_t = false)]
+    #[arg(long, help = "background layer file")]
+    layer: Option<String>,
+    #[arg(long, default_value_t = false, help = "predither the background layer")]
     predith: bool,
 }
 
@@ -27,12 +28,12 @@ fn main() {
     let mut in_file = open(args.input_file)
         .expect("could not open input file")
         .to_rgba8();
-    match args.layer.as_str() {
-        "" => {
+    match args.layer {
+        None => {
             in_file = black_dither(in_file, create_matrix(5));
         }
-        _ => {
-            let layer_file = open(args.layer)
+        Some(layer_file_path) => {
+            let layer_file = open(layer_file_path)
                 .expect("could not open layer file")
                 .to_rgba8();
             let new_dims = in_file.dimensions();
@@ -49,6 +50,7 @@ fn main() {
         .expect("could not save output file");
 }
 
+/// Create a new f32 matrix with dimensions `msize`X`msize`.
 fn create_matrix(msize: usize) -> Vec<Vec<f32>> {
     let mut v: Vec<Vec<f32>> = Vec::new();
     let mut rng = rand::rng();
@@ -62,6 +64,7 @@ fn create_matrix(msize: usize) -> Vec<Vec<f32>> {
     v
 }
 
+/// Dither `image` using the dithering matrix `matrix`.
 fn black_dither(mut image: RgbaImage, matrix: Vec<Vec<f32>>) -> RgbaImage {
     let msize = matrix.len();
     for (x, y, p) in image.enumerate_pixels_mut() {
@@ -73,6 +76,8 @@ fn black_dither(mut image: RgbaImage, matrix: Vec<Vec<f32>>) -> RgbaImage {
     image
 }
 
+/// Dither `image` using the `layer` image as the background layer
+/// with dithering matrix `matrix`.
 fn layer_dither(mut image: RgbaImage, layer: RgbaImage, matrix: Vec<Vec<f32>>) -> RgbaImage {
     assert!(image.dimensions() == layer.dimensions());
     let msize = matrix.len();
