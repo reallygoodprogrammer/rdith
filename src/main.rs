cdf016f560a6188a484774d7605c4a6d12070c16
reallygoodprogrammer <milesdustin45@gmail.com>
2025-06-22 12:43:56 -0700

    added dithering dimension and resolution control

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,46 @@ use rand::Rng;
 #[derive(Parser)]
 struct Args {
     #[arg(help = "input files for dithering")]
-    input_file: String,
+    input_files: Vec<String>,
     #[arg(long, default_value_t = String::from("output.png"), help = "name of output file")]
     output_file: String,
-    #[arg(long, help = "background layer file")]
-    layer: Option<String>,
     #[arg(long, default_value_t = false, help = "predither the background layer")]
     predith: bool,
+    #[arg(
+        short,
+        long,
+        default_value_t = 5,
+        help = "dimensions of dithering matrix"
+    )]
+    dims_matrix: usize,
+    #[arg(
+        short,
+        long,
+        default_value_t = 1,
+        help = "resolution of dithering matrix"
+    )]
+    res_matrix: usize,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let mut in_file = open(args.input_file)
+    let mut in_file = open(args.input_files[0].clone())
         .expect("could not open input file")
         .to_rgba8();
-    match args.layer {
-        None => {
-            in_file = black_dither(in_file, create_matrix(5));
-        }
-        Some(layer_file_path) => {
-            let layer_file = open(layer_file_path)
-                .expect("could not open layer file")
-                .to_rgba8();
-            let new_dims = in_file.dimensions();
-            let mut layer_file = resize(&layer_file, new_dims.0, new_dims.1, Lanczos3);
-            if args.predith {
-                layer_file = black_dither(layer_file, create_matrix(5));
-            }
-            in_file = layer_dither(in_file, layer_file, create_matrix(5));
-        }
+    if args.predith {
+        in_file = black_dither(in_file, create_matrix(args.dims_matrix, args.res_matrix));
+    }
+
+    for file in &args.input_files[1..] {
+        let layer_file = open(file).expect("could not open layer file").to_rgba8();
+        let new_dims = in_file.dimensions();
+        let layer_file = resize(&layer_file, new_dims.0, new_dims.1, Lanczos3);
+        in_file = layer_dither(
+            in_file,
+            layer_file,
+            create_matrix(args.dims_matrix, args.res_matrix),
+        );
     }
 
     in_file
@@ -51,13 +61,20 @@ fn main() {
 }
 
 /// Create a new f32 matrix with dimensions `msize`X`msize`.
-fn create_matrix(msize: usize) -> Vec<Vec<f32>> {
+fn create_matrix(msize: usize, mres: usize) -> Vec<Vec<f32>> {
     let mut v: Vec<Vec<f32>> = Vec::new();
     let mut rng = rand::rng();
     for _ in 0..msize {
         let mut row: Vec<f32> = Vec::new();
         for _ in 0..msize {
-            row.push(255.0 * rng.random::<f32>());
+            let val = 255.0 * rng.random::<f32>();
+            for _ in 0..(mres - 1) {
+                row.push(val);
+            }
+            row.push(val);
+        }
+        for _ in 0..(mres - 1) {
+            v.push(row.clone());
         }
         v.push(row);
     }
